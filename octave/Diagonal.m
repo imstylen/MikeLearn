c1350df6fd2cfa7472c7fd302d9f2ff95c1bc96d
imstylen <michael.steijlen@gmail.com>
2020-05-26 18:37:53 -0400

    added octave examples

---
+++ octave/Diagonal.m
@@ -0,0 +1,62 @@
+clear;clc;close all;
+
+s = @(z) 1./(1+exp(-z));
+ds = @(z) s(z).*(1-s(z));
+
+d1 = [1;0;0;1]
+d2 = [0;1;1;0]
+
+ W1 = rand(3,4)
+ W2 = rand(1,3)
+ 
+ B1 = rand(3,1)
+ B2 = rand(1)
+    
+ index = 1
+ 
+ for epoch = 1:200001
+     for i = 1:1
+
+        x0 = round(rand(4,1));
+        
+        if x0 == d1 || x0 == d2
+          t = 1;
+        else
+          t = 0;
+        end
+
+        x1N = W1*x0 + B1;
+        x1 = s(x1N);
+
+        x2N = W2*x1 + B2;
+        x2 = s(x2N);
+
+        delta2 = (x2-t)*ds(x2N);
+        delta1 = W2'*delta2.*ds(x1N);
+
+        dedw2 = delta2*x1';
+        dedw1 = delta1*x0';
+
+        alpha = 0.1;
+
+        W2 = W2 - alpha*dedw2;
+        W1 = W1 - alpha*dedw1;
+
+        B2 = B2 - alpha*delta2;
+        B1 = B1 - alpha*delta1;
+
+     end
+     
+     if mod(epoch,100) == 0
+         E(index) = norm(x2-t)^2;
+         E(index)
+         index = index + 1;
+         
+     end
+     
+ end
+ 
+ plot(E)
+ title("Error")
+ Predict = @(I) s(W2*s(W1*I + B1)+B2)
+ 
---
+++ octave/XOR.m
@@ -0,0 +1,63 @@
+clear;clc;close all;
+
+s = @(z) 1./(1+exp(-z));
+ds = @(z) s(z).*(1-s(z));
+
+X = [0 0;
+     0 1;
+     1 0;
+     1 1]
+Y = [0;
+     1;
+     1;
+     0]
+ 
+ W1 = rand(2,2)
+ W2 = rand(1,2)
+ 
+ B1 = rand(2,1)
+ B2 = rand(1)
+    
+ index = 1
+ 
+ for epoch = 1:20001
+     for i = 1:length(X)
+
+        x0 = X(i,:)';
+        t = Y(i);
+
+        x1N = W1*x0 + B1;
+        x1 = s(x1N);
+
+        x2N = W2*x1 + B2;
+        x2 = s(x2N);
+
+        delta2 = (x2-t)*ds(x2N);
+        delta1 = W2'*delta2.*ds(x1N);
+
+        dedw2 = delta2*x1';
+        dedw1 = delta1*x0';
+
+        alpha = 0.1;
+
+        W2 = W2 - alpha*dedw2;
+        W1 = W1 - alpha*dedw1;
+
+        B2 = B2 - alpha*delta2;
+        B1 = B1 - alpha*delta1;
+
+     end
+     
+     if mod(epoch,100) == 0
+         E(index) = norm(x2-t)^2;
+         E(index)
+         index = index + 1;
+         
+     end
+     
+ end
+ 
+ plot(E)
+ title("Error")
+ Predict = @(I) s(W2*s(W1*I + B1)+B2)
+ 
