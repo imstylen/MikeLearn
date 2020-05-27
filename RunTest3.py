8f97f7151cd1b2903c764b96f9ac9be3886e00c3
imstylen <michael.steijlen@gmail.com>
2020-05-27 18:13:53 -0400

    Update RunTest3.py

--- RunTest3.py
+++ RunTest3.py
@@ -27,7 +27,7 @@ print(len(Y))
 print("Learning")
 #Model(std::vector<float> xIn,int nInputs, std::vector<float> yIn,int nOutputs,int nHidden);
 M = MikeLearn.Model(X,nIn,Y,nOut,350)
-FinalE = M.fit(3,0.75)
+FinalE = M.fit(6,0.75)
 print("Final Error: " + str(FinalE[0]))
 
 
