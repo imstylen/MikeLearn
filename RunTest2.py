878fed5bf22cf7a977fabd263232348d8968432a
imstylen <michael.steijlen@gmail.com>
2020-05-27 16:43:18 -0400

    Update RunTest2.py

--- RunTest2.py
+++ RunTest2.py
@@ -1,6 +1,16 @@
 from Release import MikeLearn
 import random
 
+#=======================================================
+# Problem Statement
+#=======================================================
+# Create a nueral network to differentiate between 
+# diagonal linesand non-diagonal lines in  2X2 of pixels.
+
+#=======================================================
+# Training Set
+#=======================================================
+
 X = []
 Y = []
 
@@ -10,15 +20,28 @@ for i in range(0,100):
 		x.append(random.randint(0,1))
 	if x == [1,0,0,1] or x == [0,1,1,0]:
 		Y.append(1)
+		Y.append(0)
 	else:
 		Y.append(0)
+		Y.append(1)
 	for num in x:
 		X.append(num)
 
+#=======================================================
+# Fit Model
+#=======================================================
+# 4 input layer
+# 3 hidden layer
+# 2 output layer
+
 #Model(std::vector<float> xIn,int nInputs, std::vector<float> yIn,int nOutputs,int nHidden);
-M = MikeLearn.Model(X,4,Y,1,3)
-FinalE = M.fit(1000,0.75)
+M = MikeLearn.Model(X,4,Y,2,3)
+FinalE = M.fit(10000,0.75)
 print("Final Error: " + str(FinalE[0]))
+
+#=======================================================
+# Predict
+#=======================================================
 
 print(M.predict([0,0,0,0]))
 print(M.predict([0,1,1,0]))
