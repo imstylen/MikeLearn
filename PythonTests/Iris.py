c22ad0f802aabc6ddf7f52a4bfb37d57b6a915e2
imstylen <michael.steijlen@gmail.com>
2020-06-02 20:17:52 -0400

    update

--- PythonTests/Iris.py
+++ PythonTests/Iris.py
@@ -1,58 +1,50 @@
 from MikeLearn import NeuralNetwork
 from MikeLearn import ClassificationOptimizer
-import matplotlib.pyplot as plt
-import numpy as np
-import pickle
+import random
 import time
 
 #=======================================================
 # Data Set
 #=======================================================
 
-#load the data dictionary
-modeldata = pickle.load( open( "mnist_data.p", "rb" ) )
-X = modeldata['X']
-Y = modeldata['Y']
+data = open('iris.data','r').read().split('\n')
+data = [line.split(',') for line in data]
+random.shuffle(data)
 
-#normalize data
-X = np.array(X)
-X = X/255
-X = X.tolist()
+X = list();
+Y = list();
+
+for line in data:
+    if len(line)>1:
+        X.append([float(i) for i in line[0:4]])
+        if "setosa" in line[4]:
+            Y.append([1,0,0])
+        elif "versicolor" in line[4]:
+            Y.append([0,1,0])
+        else:
+            Y.append([0,0,1])
+
 
 #training set
-X1 = X[0:49999]
-Y1 = Y[0:49999]
+X1 = X[0:100]
+Y1 = Y[0:100]
 
 #validation set
-X2 = X[50000:59999]
-Y2 = Y[50000:59999]
+X2 = X[101:]
+Y2 = Y[101:]
 
 #number of inputs/outputs
 nIn = len(X[0]) #~750
 nOut = len(Y[0]) #=10
 
-
-thisline = ""
-for i,pixel in enumerate(X[0]):
-
-    if (i) % 28 == 0:
-        thisline = thisline + "\n"
-    if pixel > 0.1:
-        thisline = thisline + "+"
-    else:
-        thisline = thisline + "-"
-
-print(thisline)
-print(Y[0])
-
 #=======================================================
 # Model
 #=======================================================
-verbosity = 1
+verbosity = 0
 
 #Initualize neural network
 # NeuralNetwork([nInputs, nHidden1, nHidden2,..,nOutputs],['Activation1','Activation2'...]
-N = NeuralNetwork([nIn,50,nOut],['sigmoid','sigmoid'])
+N = NeuralNetwork([nIn,4,nOut],['sigmoid','sigmoid'])
 N.setLoggerVerbosity(verbosity)
 
 #Initialize optimizer
@@ -63,7 +55,7 @@ Opt.setLoggerVerbosity(verbosity)
 start_time = time.time();
 #fit data
 #fit(nEpoch,LearningRate)
-E = Opt.fit(10,0.1)
+E = Opt.fit(1000,0.1)
 print("--- %s seconds ---" % (time.time() - start_time))
 
 #================================
